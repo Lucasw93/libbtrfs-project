8e291cfdfe1a0005e3164953487d517163036ad4
Lucas Wheeler <98344298+Lucasw93@users.noreply.github.com>
2026-08-28 19:51:57 -0400

    Update syscalls macro

--- libbtrfs/src/macros.rs
+++ libbtrfs/src/macros.rs
@@ -38,16 +38,15 @@ macro_rules! try_or_some_err {
 macro_rules! syscall {
     ($name:ident($( $args:expr ),* $(,)?)) => {
         {
-            #[cfg(not(feature = "use-syscalls"))]
-            match ::libc::$name($( $args ),*) {
-                -1 => Err(::std::io::Error::last_os_error()),
-                res => Ok(res)
-            }
-
             #[cfg(feature = "use-syscalls")]
-            match __use_syscalls!($name($( $args ),*)) {
+            match syscall!(@$name( $($args),* )) {
                 Err(e) => Err(::std::io::Error::from_raw_os_error(e.into_raw())),
                 Ok(res) => Ok(res as ::libc::c_int),
+            }
+            #[cfg(not(feature = "use-syscalls"))]
+            match ::libc::$name( $($args),* ) {
+                -1 => Err(::std::io::Error::last_os_error()),
+                res => Ok(res)
             }
         }
     };
@@ -55,75 +54,231 @@ macro_rules! syscall {
     (unsafe { $name:ident( $( $args:expr ),*  $(,)?) }) => {
         unsafe { syscall!($name($( $args ),*)) }
     };
-}
 
-// syscall signature definitions to match libc
-#[cfg(feature = "use-syscalls")]
-macro_rules! __use_syscalls {
-    (fstat ($fd:expr, $statbuf:expr)) => {
-        ::syscalls::syscall!(
+    // =======================================================================
+    // Define system calls to match libc signatures
+    // =======================================================================
+
+    (@fallocate ( $fd:expr, $mode:expr, $offset:expr, $len:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::fallocate,
+            $fd as ::libc::c_int,
+            $mode as ::libc::c_int,
+            $offset as ::libc::off_t,
+            $len as ::libc::off_t
+        }
+    };
+
+    (@fchmodat ( $dirfd:expr, $pathname:expr, $mode:expr, $flags:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::fchmodat,
+            $dirfd as ::libc::c_int,
+            $pathname as *const ::libc::c_char,
+            $mode as ::libc::mode_t,
+            $flags as ::libc::c_int
+        }
+    };
+
+    (@fchownat ( $dirfd:expr, $pathname:expr, $owner:expr, $group:expr, $flags:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::fchownat,
+            $dirfd as ::libc::c_int,
+            $pathname as *const ::libc::c_char,
+            $owner as ::libc::uid_t,
+            $group as ::libc::gid_t,
+            $flags as ::libc::c_int
+        }
+    };
+
+    (@fcntl ( $fd:expr, $cmd:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::fcntl,
+            $fd as ::libc::c_int,
+            $cmd as ::libc::c_int,
+        }
+    };
+
+    (@fcntl ( $fd:expr, $cmd:expr, $arg:expr)) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::fcntl,
+            $fd as ::libc::c_int,
+            $cmd as ::libc::c_int,
+            $arg as ::libc::c_int
+        }
+    };
+
+    (@fstat ( $fd:expr, $statbuf:expr )) => {
+        ::syscalls::syscall! {
             ::syscalls::Sysno::fstat,
             $fd as ::libc::c_int,
             $statbuf as *mut ::libc::stat
-        )
+        }
     };
 
-    (fstatfs ($fd:expr, $buf:expr)) => {
-        ::syscalls::syscall!(
+    (@ftruncate ( $fd:expr, $length:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::ftruncate,
+            $fd as ::libc::c_int,
+            $length as ::libc::off_t
+        }
+    };
+
+    (@fstatfs ( $fd:expr, $buf:expr )) => {
+        ::syscalls::syscall! {
             ::syscalls::Sysno::fstatfs,
             $fd as ::libc::c_int,
             $buf as *mut ::libc::statfs
-        )
+        }
     };
 
-    (ioctl ($fd:expr, $op:expr, $argp:expr)) => {
-        ::syscalls::syscall!(
+    (@ioctl ( $fd:expr, $op:expr, $argp:expr )) => {
+        ::syscalls::syscall! {
             ::syscalls::Sysno::ioctl,
             $fd as ::libc::c_int,
             $op as ::libc::Ioctl,
             $argp as *mut _
-        )
+        }
     };
 
-    (open ($path:expr, $flags:expr)) => {
-        ::syscalls::syscall!(
+    (@linkat ( $olddirfd:expr, $oldpath:expr, $newdirfd:expr, $newpath:expr, $flags:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::linkat,
+            $olddirfd as ::libc::c_int,
+            $oldpath as *const ::libc::c_char,
+            $newdirfd as ::libc::c_int,
+            $newpath as *const ::libc::c_char,
+            $flags as ::libc::c_int
+        }
+    };
+
+    (@lremovexattr ( $path:expr, $name:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::lremovexattr,
+            $path as *const ::libc::c_char,
+            $name as *const ::libc::c_char
+        }
+    };
+
+    (@lsetxattr ( $path:expr, $name:expr, $value:expr, $size:expr, $flags:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::lsetxattr,
+            $path as *const ::libc::c_char,
+            $name as *const ::libc::c_char,
+            $value as *const ::libc::c_void,
+            $size as ::libc::size_t,
+            $flags as  ::libc::c_int
+        }
+    };
+
+    (@mknodat ( $dirfd:expr, $pathname:expr, $mode:expr, $dev:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::mknodat,
+            $dirfd as ::libc::c_int,
+            $pathname as *const ::libc::c_char,
+            $mode as ::libc::mode_t,
+            $dev as ::libc::dev_t
+        }
+    };
+
+    (@mkdirat ( $dirfd:expr, $pathname:expr, $mode:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::mkdirat,
+            $dirfd as ::libc::c_int,
+            $pathname as *const ::libc::c_char,
+            $mode as ::libc::mode_t
+        }
+    };
+
+    (@pwrite ( $fd:expr, $buf:expr, $count:expr, $offset:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::pwrite64,
+            $fd as ::libc::c_int,
+            $buf as *const ::libc::c_void,
+            $count as ::libc::size_t,
+            $offset as ::libc::off_t
+        }
+    };
+
+    (@open ($path:expr, $flags:expr)) => {
+        ::syscalls::syscall! {
             ::syscalls::Sysno::open,
             $path as *const ::libc::c_char,
             $flags as ::libc::c_int
-        )
+        }
     };
-    (open ($path:expr, $flags:expr, $mode:expr)) => {
-        ::syscalls::syscall!(
+
+    (@open ($path:expr, $flags:expr, $mode:expr)) => {
+        ::syscalls::syscall {
             ::syscalls::Sysno::open,
             $path as *const ::libc::c_char,
             $flags as ::libc::c_int,
             $mode as ::libc::mode_t
-        )
+        }
     };
 
-    (openat ($dirfd:expr, $path:expr, $flags:expr)) => {
-        ::syscalls::syscall!(
+    (@openat ($dirfd:expr, $path:expr, $flags:expr)) => {
+        ::syscalls::syscall! {
             ::syscalls::Sysno::openat,
             $dirfd as ::libc::c_int,
             $path as *const ::libc::c_char,
             $flags as ::libc::c_int
-        )
+        }
     };
-    (openat ($dirfd:expr, $path:expr, $flags:expr, $mode:expr)) => {
-        ::syscalls::syscall!(
+
+    (@openat ($dirfd:expr, $path:expr, $flags:expr, $mode:expr)) => {
+        ::syscalls::syscall! {
             ::syscalls::Sysno::openat,
             $dirfd as ::libc::c_int,
             $path as *const ::libc::c_char,
             $flags as ::libc::c_int,
             $mode as ::libc::mode_t
-        )
+        }
     };
 
-    (statfs ($path:expr, $buf:expr)) => {
-        ::syscalls::syscall!(
+    (@renameat2 ($olddirfd:expr, $oldpath:expr, $newdirfd:expr, $newpath:expr, $flags:expr)) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::renameat2,
+            $olddirfd as ::libc::c_int,
+            $oldpath as *const ::libc::c_char,
+            $newdirfd as ::libc::c_int,
+            $newpath as *const ::libc::c_char,
+            $flags as ::libc::c_uint
+        }
+    };
+
+    (@statfs ( $path:expr, $buf:expr )) => {
+        ::syscalls::syscall! {
             ::syscalls::Sysno::statfs,
             $path as *const ::libc::c_char,
             $buf as *mut ::libc::statfs
-        )
+        }
+    };
+
+    (@symlinkat ( $target:expr, $newdirfd:expr, $linkpath:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::symlinkat,
+            $target as *const ::libc::c_char,
+            $newdirfd as ::libc::c_int,
+            $linkpath as *const ::libc::c_char
+        }
+    };
+
+    (@unlinkat ( $dirfd:expr, $pathname:expr, $flags:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::unlinkat,
+            $dirfd as ::libc::c_int,
+            $pathname as *const ::libc::c_char,
+            $flags as ::libc::c_int
+        }
+    };
+
+    (@utimensat ( $dirfd:expr, $path:expr, $times:expr, $flags:expr )) => {
+        ::syscalls::syscall! {
+            ::syscalls::Sysno::utimensat,
+            $dirfd as ::libc::c_int,
+            $path as *const ::libc::c_char,
+            $times as *const ::libc::timespec,
+            $flags as ::libc::c_int
+        }
     };
 }
